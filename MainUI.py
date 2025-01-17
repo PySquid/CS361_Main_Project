8dcd8a9203a4291204401747b474359e6f0f3908
PySquid <sheridap@oregonstate.edu>
2025-01-17 15:59:00 -0500

    Main UI starter code added.

---
+++ MainUI.py
@@ -0,0 +1,13 @@
+# UserInterface.py
+# Author: Philip Sheridan
+# Class: CS361 / Oregon State University
+# Description: Provides User Interface for Main Project, calls Catalogue/Authorization/Profile/Log Microservices
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Execute Program
+if __name__ == '__main__':
+    print_hi('sheridap')
